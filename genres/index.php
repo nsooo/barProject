c787ee1bafa79a4f957eaac97a082f112c8ecf3d
nsooo <dennispersson1991@gmail.com>
2019-05-08 22:31:15 +0200

    Added a comment to make it easier to switch from localhost to deployment

--- genres/index.php
+++ genres/index.php
@@ -1,5 +1,6 @@
 <?php
   $url = 'http://localhost/bartending/api/genres/read.php';
+  //$url = 'http://watsaq.se/api/genres/read.php';
   
   $content = file_get_contents($url);
   $content_array = json_decode($content, true);
